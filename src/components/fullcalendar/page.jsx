56b7ead2396470f34a7aa7dc72a2aa75e9a5de50
NeiJunio <neijunio50@gmail.com>
2024-06-03 00:19:40 -0300

    modificando modal evento

--- src/components/fullcalendar/page.jsx
+++ src/components/fullcalendar/page.jsx
@@ -9,98 +9,102 @@ import ptLocale from '@fullcalendar/core/locales/pt-br';
 import styles from './page.module.css';
 
 const FullCalendar = () => {
-  const calendarRef = useRef(null);
-  const [events, setEvents] = useState([
-    { id: '1', title: 'Churrasco de japones', start: '2024-05-25T09:00:00', end: '2024-05-25T14:00:00', backgroundColor: '#FF9D00', textColor: '#000', savedAt: new Date().toISOString() },
-    { id: '2', title: 'Aniversário do Gabrielelel', start: '2024-05-10', savedAt: new Date().toISOString() }
-  ]);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  const [showModal, setShowModal] = useState(false);
+   
+    const calendarRef = useRef(null);
+    const [events, setEvents] = useState([
+        { id: '1', title: 'Churrasco de japones', start: '2024-05-25T09:00:00', end: '2024-05-25T14:00:00', backgroundColor: '#FF9D00', textColor: '#000', savedAt: new Date().toISOString() },
+        { id: '2', title: 'Aniversário do Gabrielelel', start: '2024-05-10', savedAt: new Date().toISOString() }
+    ]);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [showModal, setShowModal] = useState(false);
 
-  const handleDateClick = (arg) => {
-    const title = prompt('Digite o título do evento:');
-    if (title) {
-      const newEvent = {
-        id: String(events.length + 1), // Gera um ID único
-        title: title,
-        start: arg.date,
-        allDay: true,
-        savedAt: new Date().toISOString()
-      };
-      setEvents([...events, newEvent]);
-    }
-  };
-
-  const handleEventClick = (info) => {
-    setSelectedEvent(info.event);
-    setShowModal(true);
-  };
-
-  const handleEditClick = () => {
-    const eventId = selectedEvent.id;
-    const eventTitle = selectedEvent.title;
-    const newTitle = prompt('Digite o novo título do evento:', eventTitle);
-    if (newTitle) {
-      const updatedEvents = events.map(event =>
-        event.id === eventId ? { ...event, title: newTitle, savedAt: new Date().toISOString() } : event
-      );
-      setEvents(updatedEvents);
-      setShowModal(false);
-    }
-  };
-
-  const handleDeleteClick = () => {
-    const eventId = selectedEvent.id;
-    const eventTitle = selectedEvent.title;
-    if (window.confirm(`Tem certeza que deseja deletar o evento "${eventTitle}"?`)) {
-      const updatedEvents = events.filter(event => event.id !== eventId);
-      setEvents(updatedEvents);
-      setShowModal(false);
-    }
-  };
-
-  useEffect(() => {
-    const calendar = new Calendar(calendarRef.current, {
-      selectable: true,
-      locale: ptLocale,
-      timeZone: 'local',
-      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-      initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'dayGridMonth,timeGridWeek,timeGridDay',
-        center: 'title',
-        right: 'today prev,next'
-      },
-      events: events,
-      dateClick: handleDateClick,
-      eventClick: handleEventClick,
-    });
-
-    calendar.render();
-
-    return () => {
-      calendar.destroy();
+    const handleDateClick = (arg) => {
+        const title = prompt('Digite o título do evento:');
+        if (title) {
+            const newEvent = {
+                id: String(events.length + 1), // Gera um ID único
+                title: title,
+                start: arg.date,
+                allDay: true,
+                savedAt: new Date().toISOString()
+            };
+            setEvents([...events, newEvent]);
+        }
     };
-  }, [events]);
 
-  return (
-    <div>
-      <div ref={calendarRef} className={styles.calendar}></div>
-      {showModal && (
-        <div className={styles.modal}>
-          <div className={styles.modalContent}>
-            <h2>{selectedEvent.title}</h2>
-            <p>Início: {new Date(selectedEvent.start).toLocaleString()}</p>
-            {selectedEvent.end && <p>Fim: {new Date(selectedEvent.end).toLocaleString()}</p>}
-            <p>Salvo em: {new Date(selectedEvent.extendedProps.savedAt).toLocaleString()}</p>
-            <button onClick={handleEditClick}>Editar</button>
-            <button onClick={handleDeleteClick}>Excluir</button>
-            <button onClick={() => setShowModal(false)}>Fechar</button>
-          </div>
+    const handleEventClick = (info) => {
+        setSelectedEvent(info.event);
+        setShowModal(true);
+    };
+
+    const handleEditClick = () => {
+        const eventId = selectedEvent.id;
+        const eventTitle = selectedEvent.title;
+        const newTitle = prompt('Digite o novo título do evento:', eventTitle);
+        if (newTitle) {
+            const updatedEvents = events.map(event =>
+                event.id === eventId ? { ...event, title: newTitle, savedAt: new Date().toISOString() } : event
+            );
+            setEvents(updatedEvents);
+            setShowModal(false);
+        }
+    };
+
+    const handleDeleteClick = () => {
+        const eventId = selectedEvent.id;
+        const eventTitle = selectedEvent.title;
+    if (window.confirm(`Tem certeza que deseja deletar o evento "${eventTitle}"?`)) {
+            const updatedEvents = events.filter(event => event.id !== eventId);
+            setEvents(updatedEvents);
+            setShowModal(false);
+        }
+    };
+
+    useEffect(() => {
+        const calendar = new Calendar(calendarRef.current, {
+ 
+           contentHeight: 600,
+            handleWindowResize: true,
+            selectable: true,
+            locale: ptLocale,
+            timeZone: 'local',
+            plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'dayGridMonth,timeGridWeek,timeGridDay',
+                center: 'title',
+                right: 'today prev,next'
+            },
+            events: events,
+            dateClick: handleDateClick,
+            eventClick: handleEventClick,
+        });
+
+        calendar.render();
+
+        return () => {
+            calendar.destroy();
+        };
+    }, [events]);
+
+    return (
+        <div>
+            <div ref={calendarRef} className={styles.calendar}></div>
+            {showModal && (
+                <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <h2>{selectedEvent.title}</h2>
+                        <p>Início: {new Date(selectedEvent.start).toLocaleString()}</p>
+                        {selectedEvent.end && <p>Fim: {new Date(selectedEvent.end).toLocaleString()}</p>}
+                        <p>Salvo em: {new Date(selectedEvent.extendedProps.savedAt).toLocaleString()}</p>
+                        <button onClick={handleEditClick}>Editar</button>
+                        <button onClick={handleDeleteClick}>Excluir</button>
+                        <button onClick={() => setShowModal(false)}>Fechar</button>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default FullCalendar;
