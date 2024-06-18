3fecb114aa746ccb942937a5b95860918614bde5
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-06-18 13:30:47 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,8 @@ import React from "react";
 import { useState } from 'react';
 import Link from "next/link";
 import modal from "./filtrage/page.jsx";
-import { background, filter } from '@chakra-ui/react';
-import { color } from 'framer-motion';
 import { input } from '@nextui-org/react';
+import withReactContent from 'sweetalert2-react-content'
 
 // import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 // import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, CloseButton, Button, useDisclosure} from "@nextui-org/react";
@@ -36,6 +35,7 @@ export default function CadCliente() {
 
 
 
+
     const Pesquisar = () => {
         Swal.fire({
             width: 1200,
@@ -53,11 +53,6 @@ export default function CadCliente() {
             customClass: {
                 filter: 'my-swal',
             }
-
-
-            
-
-            
         });
     }
 
@@ -76,107 +71,42 @@ export default function CadCliente() {
             inputPlaceholder: "Enter your email address",
             showCloseButton: "true",
             backdrop: `
-        rgba(56,79,126,0.8)   
-        `,
+            rgba(56,79,126,0.8)   
+            `,
             customClass: {
                 filter: 'my-swal'
             }
-
-
         });
     }
-    //     const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    //     const [backdrop, setBackdrop] = React.useState('opaque');
 
-    // const backdrops = ["opaque","blur","transparent"];
+    const Consulta = () => {
+        Swal.fire({
+            width: 1200,
+            confirmButtonText: 'Finalizar',
+            html: `
+                <div class="consulta">
 
-    // const handleOpen = (backdrop) =>{
-    //   setBackdrop(backdrop)
-    //   onOpen();
-    // }
+                </div>
+            `
+        });
+    }
 
     return (
 
 
         <div id="clientes" className={`${styles.content_section}`}>
-            <h2>Gerenciamento de Clientes</h2>
             <div className={styles.button_group}>
                 <button id="novoCliente" onClick={Novo}>Novo</button>
-                <button id="alterarCliente">Alterar</button>
-                <button id="excluirCliente">Excluir</button>
                 {/* <button id="localizarCliente" onclick={onOpen}>Localizar</button> */}
                 <button id="localizarCliente" onClick={Pesquisar}>Localizar</button>
                 {/* <button id="localizarCliente" onClick={openModal}>Localizar</button> */}
+                <button id="localizarCliente" onClick={Consulta}>Consulta</button>
             </div>
 
             <Link href="/filtrage">pesquisars</Link>
 
 
-            <form id="clienteForm" className={styles.form}>
 
-                <input type="hidden" id="clienteId" className={styles.input_cliente} />
-
-                <div className={styles.grid}>
-                    <div className={`${styles.grid_item} ${styles.grid_item_small_2}`}>
-                        <label for="codigo_cliente" className={styles.label_cliente}>Código</label>
-                        <input type="number" id="codigo_cliente" name="codigo_cliente" required className={styles.input_cliente} />
-                    </div>
-
-                    <div className={`${styles.grid_item} ${styles.grid_item_large}`}>
-                        <label for="nome_cliente" className={styles.label_cliente} >Nome:</label>
-                        <input type="text" id="nome_cliente" name="nome_cliente" required className={styles.input_cliente} placeholder="Nome Completo" />
-                    </div>
-
-                    <div className={`${styles.grid_item} ${styles.grid_item_medium}`}>
-                        <label for="cpf_cliente" className={styles.label_cliente}>CPF:</label>
-                        <input type="text" id="cpf_cliente" name="cpf_cliente" required className={styles.input_cliente} placeholder="xxx.xxx.xxx - xx" />
-                    </div>
-                </div>
-
-                <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                    <label for="data_nasc_cliente" className={styles.label_cliente}>Data de nascimento:</label>
-                    <input type="date" id="data_nasc_cliente" name="data_nasc_cliente" required className={styles.input_cliente} />
-                </div>
-
-                <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                    <label for="sexo_cliente" className={styles.label_cliente}>Sexo:</label>
-                    <select id="sexo_cliente" name="sexo_cliente" required className={`${styles.select_cliente} ${styles.input_small_select}`}>
-                        <option value="" disabled selected>Selecionar</option>
-                        <option value="feminino">Feminino</option>
-                        <option value="masculino">Masculino</option>
-                        <option value="outro">Outro</option>
-                    </select>
-                </div>
-
-                <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                    <label for="nivel_acesso" className={styles.label_cliente}>Nível de Acesso:</label>
-                    <select id="nivel_acesso" name="nivel_acesso"
-                        className={`${styles.select_cliente} ${styles.input_small_select}`}>
-                        <option value="user" className={styles.option}>Usuário</option>
-                        <option value="admin" className={styles.option}>Administrador</option>
-                    </select>
-                </div>
-
-
-                <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                    <label for="telefone_cliente" className={styles.label_cliente}>Telefone:</label>
-                    <input type="tel" id="telefone_cliente" name="telefone_cliente" required className={` ${styles.input_cliente}`} placeholder="(xx) xxxxx - xxxxx" />
-                </div>
-
-                <div className={`${styles.grid_item} ${styles.grid_item_observacoes}`}>
-                    <label for="email_cliente" className={styles.label_cliente}>Email:</label>
-                    <input type="email_cliente" id="email_cliente" name="email_cliente" required className={styles.input_cliente} placeholder="exemplo@exemplo.com" />
-                </div>
-                <div className={`${styles.grid_item}  ${styles.grid_item_observacoes}`}>
-                    <label htmlFor="observacoes_cliente" className={styles.label_cliente}>Observações</label>
-                    <input type="text" id="observacoes_cliente" name="observacoes_cliente" required className={styles.input_cliente} />
-                </div>
-            </form>
-
-            <div className={styles.footer_form}>
-                <button type="button" onclick="cancelarAcao()" className={styles.button_cancel}>Cancelar</button>
-                <button type="submit" className={styles.button_submit}>Salvar</button>
-            </div>
 
             {/* MODAL */}
 
@@ -188,11 +118,11 @@ export default function CadCliente() {
 
                 {isOpen && (
                     <div className={styles.modal_overlay} onClick={handleOverlayClick} >
-                        
+
                         {/* <button onClick={closeModal} className={styles.btnClose}>Close</button> */}
                         <div className={styles.modal}>
-                        {/* <button onClick={closeModal} className={styles.btnClose}>Close</button> */}
-                           
+                            {/* <button onClick={closeModal} className={styles.btnClose}>Close</button> */}
+
                             <div className={styles.lorem}>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eligendi libero corrupti ullam, amet voluptate perferendis repudiandae optio distinctio aperiam, aspernatur inventore repellat? Minima ut iste sapiente cum ex optio.
                             </div>
